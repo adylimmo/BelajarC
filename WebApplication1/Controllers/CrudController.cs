7647aae2c6f5a4de6e452f4cfac8b288c105370f
Ady <ady.limmo@gmail.com>
2019-03-01 14:53:43 +0700

    Crud Pertama C#

---
+++ WebApplication1/Controllers/CrudController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CrudController : Controller
+    {
+        // GET: Crud
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CrudCon()
+        {
+            ViewBag.Message = "Your Crud page.";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+    }
+}
